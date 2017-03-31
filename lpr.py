e46500d3a294d07a87520e7ac38a667b5a30da40
hdq <1098171542@qq.com>
2017-03-31 21:00:23 +0800

    first

---
+++ lpr.py
@@ -0,0 +1,296 @@
+#!/usr/bin/env python
+# coding=utf-8
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt 
+from math import sqrt,cos
+import time
+import sys
+import tools
+import scipy.io as sio
+###################################
+#######  Local the license  #######
+###################################
+time1=time.time()
+num=input('Please enter the number:')
+file_path='/home/hdq/chepai/%s.jpg'%num
+img=cv2.imread(file_path)
+size=img.shape
+scale=sqrt((float(size[0])*float(size[1]))/1190400)
+#print ('the size and scale:%s,%s'%(size,round(scale)))
+if size[0]*size[1]>1190400 and scale>1:
+	img=cv2.resize(img,(int(size[1]/scale),int(size[0]/scale)),interpolation=cv2.INTER_CUBIC)
+gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+gasus=cv2.GaussianBlur(gray,(5,5),0)
+#blur=cv2.medianBlur(gray,5)
+cv2.imshow('img',img)
+hrc=tools.hsvfilter(img)
+cv2.imshow('hrc',hrc)
+
+#tophat=cv2.medianBlur(tophat,5)
+edge=cv2.Sobel(gasus,-1,1,1)
+#edge=cv2.blur(edge,(3,3))
+cv2.imshow('edges',edge)
+ret,binary=cv2.threshold(edge,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+kernel1=cv2.getStructuringElement(cv2.MORPH_RECT,(30,7))
+kernel2=cv2.getStructuringElement(cv2.MORPH_RECT,(4,1))
+closing=cv2.dilate(binary,kernel2,iterations=4)
+closing=cv2.morphologyEx(closing,cv2.MORPH_CLOSE,kernel1,iterations=1)
+closing=cv2.erode(closing,kernel2,iterations=4)
+dst=cv2.bitwise_and(closing,hrc)
+kernel3=cv2.getStructuringElement(cv2.MORPH_RECT,(3,2))
+dst=cv2.dilate(dst,kernel2,iterations=1)
+dst=cv2.erode(dst,kernel2,iterations=1)
+
+#print time3-time1
+cv2.imshow('erode',closing)
+cv2.imshow('dst',dst)
+
+#Choose by the width and heigh
+contours,heirs = cv2.findContours(dst.copy(),cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+rectangle=[]
+rotation=0
+smax=0
+for tour in contours:
+	rc=cv2.boundingRect(tour)
+	msc=cv2.minAreaRect(tour)
+	if len(contours) == 1:
+		rectangle.append(rc)
+		if msc[1][1]>msc[1][0]:
+			rotation=msc[2]+90
+		else:
+			rotation=msc[2]
+		print msc
+		print rc
+	elif abs((rc[2]/rc[3])-22/7)<2.4 and rc[2]*rc[3]>max(1000,dst.shape[0]*dst.shape[1]/500):
+		print msc
+		print rc
+		if abs(max(msc[1])/min(msc[1])-3.8)<1.8:
+			temp = img[rc[1]:(rc[1]+rc[3]),rc[0]:(rc[0]+rc[2])]
+			if dst[rc[1]:rc[1]+rc[3],rc[0]:rc[0]+rc[2]].mean()>120:
+				tem = cv2.cvtColor(temp,cv2.COLOR_BGR2HSV)
+				h=tem[:,:,0].mean()
+				s=tem[:,:,1].mean()
+				if h<130 and h>85 and s>60 :
+					if len(rectangle)!=0:
+						
+						if s>smax:
+							del rectangle[0]	
+							rectangle.append(rc)
+							smax=s
+							if msc[1][1]>msc[1][0]:
+								rotation=msc[2]+90
+							else:
+								rotation=msc[2]
+					else:
+						rectangle.append(rc)
+						smax=s
+						if msc[1][1]>msc[1][0]:
+							rotation=msc[2]+90
+						else:
+							rotation=msc[2]
+#Choose by the color
+print('The rectangle has:%d'%len(rectangle))
+if len(rectangle)== 1:
+	ar = rectangle[0]
+	temp = img[ar[1]-3:(ar[1]+ar[3]+2),ar[0]-3:(ar[0]+ar[2]+3)+2]
+	cv2.imwrite('img.jpg',temp)
+else:
+	print('error!Can not locate the words!')
+
+
+
+###################################
+#######   Cut the license   #######
+###################################
+
+img=cv2.imread('img.jpg')
+cv2.imshow('iimg',img)
+cv2.waitKey(0)
+
+img=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+kernel1=cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(15,15))
+kernel2=cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(100,100))
+tw=cv2.morphologyEx(img,cv2.MORPH_TOPHAT,kernel1)
+bw=cv2.morphologyEx(img,cv2.MORPH_BLACKHAT,kernel2)
+temp=cv2.add(img,tw)
+result=cv2.subtract(temp,bw)
+ret,bw=cv2.threshold(result,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+cv2.imshow('final',result)
+
+bw=tools.rotate_about_center(bw,rotation,flag=False)
+ret,bw=cv2.threshold(bw,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+#bw=tools.PcaRotate(bw)
+cv2.imshow('rotate',bw)
+
+smallflag=False
+if abs(rotation)<5:
+	bwheigh_10=0
+	if abs(rotation)<=1 and bw.shape[0]*bw.shape[1]<5000:
+		smallflag=True
+		rotation=0
+else:
+	bwheigh_10=bw.shape[0]/20
+
+
+nepoch=0
+wordsnum=0
+while nepoch<4 and wordsnum<6:
+	bw=bw[0:bw.shape[0]-2*nepoch,:] 
+	bw=tools.locatethewords(bw,bwheigh_10,smallflag)
+	bw=tools.cuttheimg(bw,flag=False)
+
+	if bw.shape[0]*bw.shape[1]==0:
+		print'Error,could not find th words!'
+		cv2.waitKey(0)
+	words,isone,wordsnum=tools.cutthewords(bw,rotation)
+	print wordsnum
+	nepoch+=1
+cv2.imshow('qiege',bw)
+
+
+num=len(words)
+print'The word has:%s'%num
+wordsort=[words[i][0] for i in xrange(num)]
+arrayisone=np.array(isone)
+isone=arrayisone[np.array(np.argsort(wordsort))].tolist()
+print isone
+
+if num>=1:
+
+	words=sorted(words,key=lambda x:x[0])
+	while num>7:
+		if (words[1][0]-words[0][0])<(words[2][2]/2) or words[0][3]/words[0][2]>5 or (words[1][1]+words[2][1])/2-words[0][1]>words[2][3]/6:
+			del words[0]
+			del isone[0]
+			num-=1
+		else:
+			del words[num-1]
+			del isone[num-1]
+			num-=1
+	if num==7 :
+		if (words[6][0]-words[5][0]-words[5][2]<words[6][0] \
+		or (isone[5]==1 and words[6][0]-words[5][0]+words[5][2]<words[6][0]*1.5) ) and isone[6]==1 :
+			del words[num-1]
+			del isone[num-1]
+			tem=[0,0,0,0]
+			tem[0]=words[0][0]-words[0][2]*1.2
+			if tem[0]<1:
+				tem[0]=1
+			tem[1]=words[0][1]
+			start=words[1][0]-1
+			while sum(bw[:,start])==0:
+				start-=1
+			tem[2]=start-tem[0]
+			tem[3]=words[0][3]
+			tem=tuple(tem)
+			words.insert(0,tem)
+			isone.insert(0,0)
+		else:
+			tem=[0,0,0,0]
+			tem[0]=words[1][0]-words[1][2]*1.2
+			if tem[0]<1:
+				tem[0]=1
+			tem[1]=words[1][1]
+			start=words[1][0]-1
+			while sum(bw[:,start])==0:
+				start-=1
+			tem[2]=start-tem[0]
+			tem[3]=words[1][3]
+			tem=tuple(tem)
+			del words[0]
+			words.insert(0,tem)
+	elif num==6:
+		tem=[0,0,0,0]
+		tem[0]=words[0][0]-words[0][2]*1.2
+		if tem[0]<1:
+			tem[0]=1
+		tem[1]=words[0][1]
+		start=words[0][0]-1
+		while sum(bw[:,start])==0:
+			start-=1
+		tem[2]=start-tem[0]
+		tem[3]=words[0][3]
+		tem=tuple(tem)
+		words.insert(0,tem)
+		isone.insert(0,0)
+		num+=1
+
+else:
+	print('cannot find the words!!')
+print isone
+print bw.shape
+time2=time.time()
+#print time2-time1
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+fig=plt.figure()
+for i in xrange(num):
+	plt.subplot(2,num,i+1)
+	ar=words[i]
+	temp=bw[int(ar[1]):int(ar[1]+ar[3]+1),int(ar[0]):int(ar[0]+ar[2]+1)]
+
+	temp=tools.cuttheimg(temp,flag=False)	
+	plt.imshow(temp,cmap='gray')
+	plt.axis("off")
+	temp=cv2.resize(temp,(14,28),interpolation=cv2.INTER_AREA)
+
+	cv2.imwrite('%s.jpg'%(i+1),temp)
+
+if num<7:
+	print 'Your should enter 7 words!'
+	sys.exit(0)
+
+###################################
+##### recongnite the license ######
+###################################
+input_vector=[]
+for i in xrange(7):
+	plt.subplot(2,num,i+8)
+	file_path='%s.jpg'%(i+1)
+	img=cv2.imread(file_path)
+	img=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+	ret,bw=cv2.threshold(img,0,255,200)
+	high,wide=bw.shape	
+	czeros=np.zeros((28,7),np.uint8)
+	final=np.column_stack((czeros,bw,czeros))
+	cv2.imshow('bw',bw)
+	vector=final.reshape((784,))
+	input_vector.append(vector)
+	plt.imshow(final,cmap ='gray')
+	plt.axis("off")
+plt.show()
+inputv=np.array(input_vector)
+inputv=np.transpose(inputv)
+test_x=inputv
+cnn_hanzi=sio.loadmat('cnn_hanzi.mat')
+cnn=cnn_hanzi['cnn']
+
+word=test_x[:,0].reshape((28,28))
+cnn = tools.cnnff2(cnn,word)
+h1=np.argmax(cnn['o'][0][0])
+h1=tools.get_hanzi(h1+1)
+cnn_26zimu=sio.loadmat('cnn_zimu.mat')
+cnn=cnn_26zimu['cnn']
+word=test_x[:,1].reshape((28,28))
+cnn = tools.cnnff2(cnn,word)
+h2=np.argmax(cnn['o'][0][0])
+h2=tools.get_26zimu(h2+1)
+cnn_zimu_shuzi=sio.loadmat('cnn_zimushuzi.mat')
+cnn=cnn_zimu_shuzi['cnn']
+h3_7=[0,0,0,0,0]
+
+print '\n  The number of licence plate is:'
+print h1,h2,
+for i in range(2,7):
+	if isone[i]==1:
+		h3_7[i-2]='1'
+		print h3_7[i-2],
+	else:
+		word=test_x[:,i].reshape((28,28))
+		cnn = tools.cnnff2(cnn,word)
+		h3_7[i-2]=tools.get_zimu_shuzi(np.argmax(cnn['o'][0][0])+1)
+		print h3_7[i-2],
+time3=time.time()
---
+++ tools.py
@@ -0,0 +1,356 @@
+#! usr/bin/env python
+# coding=utf-8 
+import cv2
+import numpy as np
+import math
+import scipy.io as sio
+from numpy.matlib import repmat 
+import copy
+
+def zeroMean(dataMat):        
+    meanVal=np.mean(dataMat,axis=0)    
+    newData=dataMat-meanVal  
+    return newData,meanVal 
+def percentage2n(eigvals):
+	sortarray=np.sort(eigvals)
+	sortarray=sortarray[-1::-1]
+	arraysum=sum(sortarray)
+	tmpsum=0
+	num=0
+	for i in sortarray:
+		tmpsum+=i
+		num+=1
+		if tmpsum>=arraysum*0.9:
+			return num
+
+def pca(dataMat):
+	newData,meanVal=zeroMean(dataMat) 
+	covMat=np.cov(newData,rowvar=0)
+	eigVals,eigVects=np.linalg.eig(np.mat(covMat))
+	n=percentage2n(eigVals)
+	eigValIndice=np.argsort(eigVals)
+	n_eigValIndice=eigValIndice[:-(n+1):-1] 
+	n_eigVect=eigVects[:,n_eigValIndice]       
+	return covMat,n_eigVect
+def rotate_about_center(src,rangle,flag=True,scale=1.0):
+    w=src.shape[1]
+    h=src.shape[0]
+    if flag== True:
+        rangle=math.pi/2-rangle
+        angle=np.rad2deg(rangle)
+        if angle>90:
+    	   angle=180+angle
+    else:
+        angle=rangle
+        rangle=np.deg2rad(angle)
+    nw=(abs(math.sin(rangle)*h)+abs(math.cos(rangle)*w))*scale
+    nh=(abs(math.cos(rangle)*h)+abs(math.sin(rangle)*w))*scale
+    rot_mat=cv2.getRotationMatrix2D((nw*0.5,nh*0.5),angle,scale)
+    rot_move=np.dot(rot_mat,np.array([(nw-w)*0.5,(nh-h)*0.5,0]))
+    rot_mat[0,2]+=rot_move[0]
+    rot_mat[1,2]+=rot_move[1]
+    return cv2.warpAffine(src,rot_mat,(int(math.ceil(nw)),int(math.ceil(nh))), flags=cv2.INTER_LANCZOS4)
+def PcaRotate(img):
+    h, w = img.shape[:2]
+    mask = np.zeros((h+2, w+2), np.uint8)
+    mask[:] = 0 
+    cv2.floodFill(img, mask,(w-1,1),0)
+    cv2.floodFill(img, mask,(1,h-1),0)
+    cv2.floodFill(img, mask,(w-1,h-1),0)
+    cv2.floodFill(img, mask,(1,1),0)
+    temp=np.where(img==255)
+    location=np.array(temp)
+    location=location.T
+    conmat,base=pca(location)
+    theta=math.atan(base[1,0]/base[0,0])
+    src=rotate_about_center(img,theta)
+    ret,src=cv2.threshold(src,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+    return src
+def hsvfilter(img):
+    hsv=cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
+    heigh=hsv.shape[0]
+    width=hsv.shape[1]   
+    lut1=np.zeros(256)
+    lut2=np.zeros(256)
+    lut3=np.zeros(256)
+    for i in range(0,255):
+        if i<130 and i>95:
+            lut1[i]=255
+        if i>70:
+            lut2[i]=255
+        if i>50 and i<220:
+            lut3[i]=255
+    im1=cv2.LUT(hsv[:,:,0],lut1)
+    im2=cv2.LUT(hsv[:,:,1],lut2)
+    im3=cv2.LUT(hsv[:,:,2],lut3)
+    im=cv2.bitwise_and(im1,im2)
+    ims=cv2.bitwise_and(im,im3)
+    kernel=cv2.getStructuringElement(cv2.MORPH_RECT,(4,1))
+    ims=cv2.dilate(ims,kernel,iterations=1)
+    ims=cv2.morphologyEx(ims,cv2.MORPH_CLOSE,kernel,iterations=2)
+    ims=np.uint8(ims)
+    return ims
+
+def locatethewords(img,shift=0,smallflag=False):
+    if img.shape[0]*img.shape[1]>12000:
+        kernel=(5,5)
+        thb=False
+    else:
+        kernel=(3,3)
+        thb=True
+    bw=cv2.GaussianBlur(img,kernel,0)
+    ret,bw=cv2.threshold(bw,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+    width=bw.shape[1]
+    heigh=bw.shape[0]
+    #qiege
+    yy1=0
+    yy2=heigh-1
+    if smallflag:
+        p=12-2
+    else:
+        p=12
+    center=heigh/2
+
+    for y in xrange(center):
+        npixels=0
+        lpixels=0
+        jump=0
+        for x in xrange(width):
+            if bw[heigh/2-y,x]!=jump:
+                jump=bw[center-y,x]
+                npixels+=1
+        
+        if npixels<p or (center-y)==0:
+            for x in xrange(width):
+                if bw[heigh/2-y-1,x]!=jump:
+                    jump=bw[center-y,x]
+                    lpixels+=1
+            if lpixels>5 and thb:
+                print'thbup'
+                yy1=heigh/2-y-shift
+                if yy1<0:
+                    yy1=0
+            else:
+                yy1=heigh/2-y
+            break
+
+    for y in xrange(heigh-center):
+        npixels=0
+        lpixels=0
+        jump=0
+        for x in xrange(width):
+            if bw[center+y,x]!=jump:
+                jump=bw[center+y,x]
+                npixels+=1  
+
+        if npixels<p or y==heigh/2:
+            for x in xrange(width):
+                if bw[heigh/2+y-1,x]!=jump:
+                    jump=bw[center+y,x]
+                    lpixels+=1
+            if lpixels>5 and thb:
+                print'thbdown'
+                yy2=heigh/2+y+shift
+                if yy2>heigh:
+                    yy2=heigh
+            else:
+                yy2=heigh/2+y
+            break
+
+    cbw=img[yy1:yy2,:]
+    return cbw
+def cuttheimg(img,flag=True,x=True,y=True):
+    ret,img=cv2.threshold(img,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+    sp=img.shape
+    if flag:
+        for i in xrange(sp[1]):
+            if sum(img[:,i])<=510:
+                img[:,i]=0
+
+    xx1=0
+    xx2=sp[1]-1
+    yy1=0
+    yy2=sp[0]-1
+    if y:
+        for i in xrange(sp[0]):
+            if sum(img[i,:])>255:
+                yy1=i
+                break
+        for i in xrange(sp[0]):
+            if sum(img[sp[0]-i-1,:])>255:
+                yy2=sp[0]-i-1
+                break
+    if x:
+        for j in xrange(sp[1]):
+            if sum(img[:,j])>255 :
+                xx1=j
+                break
+        for j in xrange(sp[1]):
+            if sum(img[:,sp[1]-1-j])>255 :
+                xx2=sp[1]-1-j
+                break
+    if yy1-1<0:
+        yy1=0
+    else:
+        yy1+=1
+    if xx1-1<0:
+        xx1=0
+    else:
+        xx1+=1    
+    cut=img[yy1:yy2+1,xx1:xx2+1]
+    return cut
+
+def cutthewords(bw,rotation):
+        wordsnum=0
+        bwscale=8.8*math.cos(np.deg2rad(rotation))*math.cos(np.deg2rad(rotation))/(bw.shape[1]/float(bw.shape[0]))
+        print 'the bw.shape of license is:',bw.shape
+        print 'the bwscale is:',bwscale
+        contours,heirs = cv2.findContours(bw.copy(),cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
+        for inner in contours:
+            rc=cv2.boundingRect(inner)
+            if abs(rc[3]/rc[2]-3.2)<1 and rc[3]*rc[2]>(bw.shape[1]*bw.shape[0]*1/3):
+                bw=bw[int(rc[1]):int(rc[1]+rc[3]),int(rc[0]):int(rc[0]+rc[2])]
+                
+        words=[]
+        isone=[]
+        contours,heirs = cv2.findContours(bw.copy(),cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
+        for tour in contours:
+            rc=cv2.boundingRect(tour)
+            
+            if rc[3]>rc[2] and abs((float(rc[3])/rc[2])-bwscale)<=1 and bw[rc[1]:rc[3]+rc[1],rc[0]:rc[0]+rc[2]].mean()<220 and rc[3]*rc[2]>bw.shape[0]*2:
+                words.append(rc)
+                isone.append(0)
+                wordsnum+=1
+            else:
+                msc=cv2.minAreaRect(tour)
+
+                if msc[1][1]*msc[1][0]!=0 and abs(max(msc[1])/min(msc[1])-3.8*bwscale)<=4 and \
+                    rc[0] >=(bw.shape[1]/3) and rc[2]*rc[3]>(bw.shape[0]*1.2):
+                    print msc
+                    rsc=[]
+                    rsc.append(rc[0]-rc[2]/2)
+                    rsc.append(rc[1])
+                    rsc.append(rc[2]*2)
+                    rsc.append(rc[3])
+                    rsc=tuple(rsc)
+                    words.append(rsc)
+                    isone.append(1)
+                    wordsnum+=1
+
+        return words,isone,wordsnum
+def get_hanzi(h1):
+    if h1== 1: h1='藏'
+    elif h1== 2: h1='川'
+    elif h1== 3: h1='鄂'
+    elif h1== 4: h1='甘'
+    elif h1== 5: h1='赣'
+    elif h1== 6: h1='贵'
+    elif h1== 7: h1='桂'
+    elif h1== 8: h1='黑'
+    elif h1== 9: h1='沪'
+    elif h1== 10: h1='吉'
+    elif h1== 11: h1='冀'
+    elif h1== 12: h1='津'
+    elif h1== 13: h1='晋'
+    elif h1== 14: h1='京'
+    elif h1== 15: h1='辽'
+    elif h1== 16: h1='鲁'
+    elif h1== 17: h1='蒙'
+    elif h1== 18: h1='闽'
+    elif h1== 19: h1='宁'
+    elif h1== 20: h1='青'
+    elif h1== 21: h1='琼'
+    elif h1== 22: h1='陜'
+    elif h1== 23: h1='苏'
+    elif h1== 24: h1='皖'
+    elif h1== 25: h1='湘'
+    elif h1== 26: h1='新'
+    elif h1== 27: h1='渝'
+    elif h1== 28: h1='豫'
+    elif h1== 29: h1='粤'
+    elif h1== 30: h1='云'
+    elif h1== 31: h1='浙'
+    return h1
+def get_26zimu(h2):
+    h2=chr(h2+64)
+    return h2
+
+def get_zimu_shuzi(h):
+    if h<=8:           # A ~ H
+        h=chr(h+64)
+    elif 9<=h<=13:     # J ~ N
+        h=chr(h+65)
+    elif 14<=h<=24:    # P ~ Z
+        h=chr(h+66)
+    elif h==25:
+        h=0
+    elif 26<=h<=33:     # 数字
+        h=h-24
+    return h
+
+def sigmoid(x):
+    hx=x.shape[0]
+    lx=x.shape[1]
+    res=[1/(1+math.exp(-x[i][j]))  if x[i][j]>0 else 1-1/(1+math.exp(x[i][j])) for i in range(hx) for j in range(lx)]
+    y=np.array(res).reshape((hx,lx))
+    return y 
+
+
+def conv(x,y):
+    bx=x.shape[0]
+    by=y.shape[1]
+    bz=bx-by+1
+    zs=[sum([np.convolve(x[i+k,j:j+by],y[by-1-i,],'vaild')for i in xrange(by)])for k in xrange(bz)for j in xrange(bz)]
+    z=np.array(zs).reshape((bz,bz))
+    return z
+
+def sub(x,y):
+ #x=np.array(range(25)).reshape(5,5)
+ #y=np.array(range(9)).reshape(3,3)
+    bx=x.shape[0]
+    by=y.shape[0]
+    bz=bx/by
+    zs=[sum([np.convolve(x[i+k,j:j+by],y[by-1-i,],'valid')for i in range(by)])for k in range(bx)[0:bx:by]for j in range(bx)[0:bx:by]]
+ #z=np.zeros([3,3])
+    z=np.array(zs).reshape((bz,bz))
+    return z
+
+def cnnff2(cnn,word):
+    layers=cnn['layers'][0,0]
+    conv1=layers[1,0]
+    sub1=layers[2,0]
+    conv2=layers[3,0]
+    subl2=layers[4,0]
+    n=layers.size
+    layers[0,0]['a'][0,0][0,0]=word
+    inputmaps=1
+    for i in range(1,n):
+        layer2=layers[i][0]
+        layer1=layers[i-1][0]
+        if layer2[0][0][0][0]=='c':
+            for j in xrange(layer2['outputmaps'][0,0][0,0]):
+                z=np.zeros(layer1['a'][0,0][0,0].shape[0]-layer2['kernelsize'][0,0][0,0]+1,layer1['a'][0,0][0,0].shape[0]-layer2['kernelsize'][0,0][0,0]+1)
+                for k in xrange(inputmaps):
+                    dst=copy.copy(layer1['a'][0,0][0,k-1])
+                    w=copy.copy(layer2['k'][0,0][0,k-1][0,j])
+                    z=z+conv(dst,w)                     
+                layer2['a'][0,0][0,j]=sigmoid(z+layer2['b'][0,0][0,j])
+            inputmaps=layer2['outputmaps'][0,0][0,0]
+        elif layer2[0][0][0][0]=='s':
+            for j in xrange(inputmaps):
+                dst=copy.copy(layer1['a'][0,0][0,j])
+                w=np.ones((layer2['scale'][0,0][0,0],layer2['scale'][0,0][0,0]))/pow(layer2['scale'][0,0][0,0],2)
+                layer2['a'][0,0][0,j]=sub(dst,w)
+    fv=cnn['fv'][0,0]
+    for s in range(layers[n-1,0]['a'][0,0].size):
+        if s ==0:
+            sa=layers[n-1,0]['a'][0,0][0,s].shape
+            tem=np.transpose(layers[n-1,0]['a'][0,0][0,s])
+            fv=tem.reshape((sa[0]*sa[1],1))
+        else:
+            sa=layers[n-1,0]['a'][0,0][0,s].shape
+            fv=np.row_stack((fv,np.transpose(layers[n-1,0]['a'][0,0][0,s]).reshape((sa[0]*sa[1],1))))
+    W=np.dot(cnn['ffW'][0,0],fv)
+    b=repmat(cnn['ffb'][0,0],1,fv.shape[1])
+    cnn['o'][0,0]=sigmoid(W+b)
+    return cnn  
